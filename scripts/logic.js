f9142f7160450ef130abcbabbd80d4108420ad34
Ekaterina <nataveres@gmail.com>
2020-02-05 19:01:14 +0100

    Logic

--- scripts/logic.js
+++ scripts/logic.js
@@ -16,11 +16,19 @@ const doMathTests = [
   { name: 'invalid - 1', args: ['tree', 0, 0], expected: 'invalid operation' },
   { name: 'invalid - 2', args: ['book', 0, 0], expected: 'invalid operation' },
   { name: 'invalid - 3', args: ['minus', 0, 0], expected: 'invalid operation' },
-  // write 7 more test cases for doMath
+  // write 7 more test cases for doMath:
+  { name: 'add - 4', args: ['add', -7, 8], expected: -56 },
+  { name: 'min - 4', args: ['min', 112, 111], expected: 1 },
+  { name: 'div - 4', args: ['div', 10, 4], expected: 2.5 },
+  { name: 'mul - 4', args: ['mul', 13, 2], expected: 26 },
+  { name: 'mul - 5', args: ['mul', -12, -10], expected: 120 },
+  { name: 'invalid - 4', args: ['plus', 10, 1], expected: 'invalid operation' },
+  { name: 'div - 5', args: ['div', 100, 4], expected: 25 },
 
 ];
 
 // refactor the logic from the calculator tutorial into this function
+
 function doMath(operation, a, b) {
   // these if statements make sure all arguments are the correct type
   // they will throw errors if your handler passes the wrong type arguments
@@ -34,9 +42,19 @@ function doMath(operation, a, b) {
     throw new Error('b should be a number');
   }
   // write your code below this comment:
+  var calculate;
+  
+  if (if op == "add") {
+    calculate = a + b;
+  } else if (if op == "min") {
+    calculate = a - b;
+  } else if (if op == "div") {
+    calculate = a / b;
+  } else if (if op == "mul") {
+    calculate = a * b;
+  }
 
-
-
+  return calculate;
 }
 
 
